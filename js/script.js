2d58a2f460a4e55635f4e0c2e7b6b753f9916d2f
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2025-02-17 17:02:33 +0200

    add internal links

--- js/script.js
+++ js/script.js
@@ -6,8 +6,60 @@ document.querySelector(".icon-close-menu").addEventListener("click", () => {
   document.querySelector(".nav-header-list-sm").style.display = "none";
 });
 
-document.querySelectorAll(".nav-header-link-sm").forEach((link) => {
-  link.addEventListener("click", () => {
+document.querySelector(".nav-logo").addEventListener("click", (e) => {
+  e.preventDefault();
+  const id = document.querySelector(".nav-logo").getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelectorAll(".nav-header-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = link.getAttribute("href");
+    console.log(id);
     document.querySelector(".nav-header-list-sm").style.display = "none";
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.querySelectorAll(".nav-header-link-sm").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = link.getAttribute("href");
+    console.log(id);
+    document.querySelector(".nav-header-list-sm").style.display = "none";
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.querySelectorAll(".nav-footer-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = link.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.querySelector(".btn-hero-full").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#services").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".btn-hero-empty").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#prices").scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelectorAll(".btn-portfolio").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#portfolio").scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.querySelectorAll(".btn-order").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
   });
 });
